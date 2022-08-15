d7b0bd40203a62269ea9d92bd206791ace61c992
Karl Gorgoglione <karlgorgo@Karls-MacBook-Air.local>
2022-08-16 00:28:55 +0200

    Init commit, website structure with Astro

---
+++ astro.config.mjs
@@ -0,0 +1,12 @@
+import { defineConfig } from 'astro/config';
+import tailwind from "@astrojs/tailwind";
+import partytown from "@astrojs/partytown";
+import prefetch from "@astrojs/prefetch";
+
+import image from "@astrojs/image";
+import sitemap from "@astrojs/sitemap";
+
+// https://astro.build/config
+export default defineConfig({
+  integrations: [tailwind({ config: { applyBaseStyles: false }}), partytown(), prefetch(), image(), sitemap()]
+});
